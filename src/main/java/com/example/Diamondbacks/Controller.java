f0dff4b604a74cd69b8f1b2d7c07dcd74c63887e
Timothy-Shi-CS <56142826+Timothy-Shi-CS@users.noreply.github.com>
2021-04-24 17:12:36 -0400

    Added handler methods skeleton

--- src/main/java/com/example/Diamondbacks/Controller.java
+++ src/main/java/com/example/Diamondbacks/Controller.java
@@ -1,8 +1,11 @@
 package com.example.Diamondbacks;
 
+import com.sun.org.apache.xerces.internal.impl.XMLEntityManager;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 // http://localhost:8080/Diamondbacks-1.0-SNAPSHOT/api/controller
 @Path("/controller")
@@ -12,4 +15,8 @@ public class Controller {
     public String hello() {
         return "Hello, World!";
     }
+
+//    public Response callJobHandler(String stateName){
+//
+//    }
 }
--- src/main/java/com/example/Diamondbacks/JobHandler.java
+++ src/main/java/com/example/Diamondbacks/JobHandler.java
@@ -1,4 +1,21 @@
 package com.example.Diamondbacks;
 
+import java.util.Map;
+
 public class JobHandler {
+    public String getPrelimAnalysis(Job job){
+        return null;
+    }
+    public Map<String, Float> callTopDistrictingsObjectiveFunction(Job currentJob){
+        return null;
+    }
+    public Map<String, Float> callTopDistrictingsByDeviationFromEnacted(Job currentJob){
+        return null;
+    }
+    public Map<String, Float> callDistrictingsByMajorMinorityRange(Job currentJob){
+        return null;
+    }
+    public Map<String, Float> callVeryDifferentAreaPairDeviations(Job currentJob){
+        return null;
+    }
 }
