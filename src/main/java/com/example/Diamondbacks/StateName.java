14c370df85666317ae150e7cb9b9b177875de40c
Gary Jiang <gary.jiang@stonybrook.edu>
2021-04-30 20:39:41 -0400

    Use Case 2: Now able to query by statename. statename must be capitalized. Made changes to enum class to better suit the use cases.

--- src/main/java/com/example/Diamondbacks/StateName.java
+++ src/main/java/com/example/Diamondbacks/StateName.java
@@ -1,5 +1,17 @@
 package com.example.Diamondbacks;
 
 public enum StateName {
-    UTAH, VIRGINIA, ARIZONA
+    UTAH(0),
+    VIRGINIA(1),
+    ARIZONA(2);
+
+    private Integer stateNumber;
+
+    private StateName(final Integer stateNumber){
+        this.stateNumber = stateNumber;
+    }
+
+    public Integer getStateNumber(){
+        return this.stateNumber;
+    }
 }
