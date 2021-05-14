00c805d16c700960ba15ddc1a373dccbcc9e90fd
Hui Chen <55211651+huichen2333@users.noreply.github.com>
2021-05-14 02:14:49 -0400

    applying sigmoid function to the devation from enacted/avg districting

--- src/main/java/com/example/Diamondbacks/ObjectiveValue.java
+++ src/main/java/com/example/Diamondbacks/ObjectiveValue.java
@@ -7,15 +7,24 @@ public class ObjectiveValue {
     private float overallObjectiveValueScore; //the total object value score
     private Map<MeasureType, Measure> measures; //hashmap of all possible measure types
 
+    public double sigmoidFunction(Double sse){
+        return 1/(1+Math.exp(Math.pow(-10,-25)*sse));
+    }
     /**
      * The method iterates through the measures hashmap and calculates the overall total score
      * @return overall total objective value score
      */
     public float calOverallObjectiveValueScore(){
         float total_score = 0;
-        for(Measure measure:measures.values()){
+        for(MeasureType tp: this.getMeasures().keySet()){
+            Measure measure = this.getMeasures().get(tp);
             if(measure.getMeasureWeight()>0){
-                total_score+= measure.getMeasureWeight()*measure.getMeasureScore();
+                if(tp.equals(MeasureType.DEV_AVERAGE_GEO) || tp.equals(MeasureType.DEV_AVERAGE_POP) ||
+                        tp.equals(MeasureType.DEV_ENACTED_GEO) ||tp.equals(MeasureType.DEV_ENACTED_POP)){
+                    total_score+= measure.getMeasureWeight()*sigmoidFunction(measure.getMeasureScore());
+                }else{
+                    total_score+= measure.getMeasureWeight()*measure.getMeasureScore();
+                }
             }
         }
         this.setOverallObjectiveValueScore(total_score);
